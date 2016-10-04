4ef4112e10ec839efc79fe012cc8e02b09420148
Deepa <deepa2208@gmail.com>
2016-10-03 21:10:34 -0500

    Modified changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,21 @@
 class PostsController < ApplicationController
 def index
+@posts = Post.all
+end
+def new
+@posts = Post.new
+end
+def create
+@posts = Post.create(post_params)
+redirect_to posts_path
 end
 
+
+
+
+
+private
+def post_params
+params.require(.post).permit(:image. :caption)
+end
+end 
